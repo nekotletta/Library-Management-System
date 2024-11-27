1830b608be722d7b2c9d5cad8d5d12ccbfe503d5
nekotletta <forprogrammingpurposes02@gmail.com>
2024-11-26 23:01:24 -0400

    removing unnecessary django apps

--- lms_backend/student_dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView,  RedirectView
-from .views import *
-
-urlpatterns = [
-    path('', student_main_page, name='student_main_page'),
-]
--- lms_backend/user_reg/serializers.py
+++
@@ -1,7 +0,0 @@
-# from rest_framework import serializers
-# from .models import *
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = LibraryUser
-#         fields = '__all__'
